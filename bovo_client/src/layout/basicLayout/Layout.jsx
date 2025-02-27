fa64188864509096d9dc1cdb521a8e2c1e8b1f94
Elvin-lee <herjun802@gmail.com>
2025-02-28 00:24:12 +0900

    404 error 경로 처리 및 404 페이지 구현 완료

--- bovo_client/src/layout/basicLayout/Layout.jsx
+++ bovo_client/src/layout/basicLayout/Layout.jsx
@@ -1,14 +1,15 @@
 import { Container } from "@mui/material";
 import styles from "./Layout.module.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../header/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleLogoutModal } from "../../store/logout/LogeoutSlice";
 import LogoutModal from "../../components/logoutModal/LogoutModal";
 
 const Layout = () => {
+    const location = useLocation();  // 현재 경로 가져오기
     const dispatch = useDispatch();
-    const isLogout = useSelector((state) => state.logout.isLogout);
+    const isLogout = useSelector((state) => state.logout.isLogout); //logout 모달 상태
 
     const onLogout = (state) => {
         dispatch(toggleLogoutModal(state));
@@ -16,7 +17,7 @@ const Layout = () => {
 
     return (
         <Container className={styles.layout} disableGutters>
-            <Header onLogout={onLogout}/>
+            {location.pathname !== "/404" && <Header onLogout={onLogout} />}
             <Outlet />
             {isLogout && <LogoutModal />}
         </Container>
