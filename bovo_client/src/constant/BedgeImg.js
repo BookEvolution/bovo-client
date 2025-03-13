0273a11188eb8629cedd4c6c15e17f8d23b8df51
Elvin-lee <herjun802@gmail.com>
2025-03-13 23:32:42 +0900

    feat: Add bedge 및 profileImg 경로 src 경로에서 AWS S3로 대체

---
+++ bovo_client/src/constant/BedgeImg.js
@@ -0,0 +1,13 @@
+const bedgeImages = [
+    { key: "BM", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-001.png" },
+    { key: "SM", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-002.png" },
+    { key: "GM", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-003.png" },
+    { key: "BT", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-004.png" },
+    { key: "ST", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-005.png" },
+    { key: "GT", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-006.png" },
+    { key: "BC", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-007.png" },
+    { key: "SC", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-008.png" },
+    { key: "GC", src: "https://bovo-img-bucket.s3.ap-southeast-2.amazonaws.com/medal/medal-009.png" },
+];
+
+export default bedgeImages;
