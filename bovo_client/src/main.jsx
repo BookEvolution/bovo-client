e3e4812ca00da5b4d124e3399b428fd7db426e00
Elvin-lee <herjun802@gmail.com>
2025-03-14 09:29:22 +0900

    feat: Modify msw가 불필요해짐에 따라 삭제

--- bovo_client/src/main.jsx
+++ bovo_client/src/main.jsx
@@ -1,14 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { startWorker } from './mocks/browser'
 import { Provider } from 'react-redux'
 import { store } from './store/index.js'
-
-/* 개발 환경에서 MSW 시작*/
-if (process.env.NODE_ENV === 'development') {
-  startWorker()
-}
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
