63bd3ebd39c14e43df93c038fa9fc099717e241e
Elvin-lee <herjun802@gmail.com>
2025-03-14 19:16:18 +0900

    feat: Add 토론방 생성을 위한 도서 검색시 도서 정보를 가져오는 Redux 상태관리 구현

--- bovo_client/src/store/index.js
+++ bovo_client/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logoutReducer from './logout/LogeoutSlice';
+import logoutReducer from './logout/LogoutSlice';
+import bookReducer from "./bookForum/BookSlice"; // bookSlice import
 
 export const store = configureStore({
     reducer: {
         logout: logoutReducer,
+        book: bookReducer
     }
 })
