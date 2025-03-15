beecdef14a016fa01b0c9a433199150245e76179
Elvin-lee <herjun802@gmail.com>
2025-03-16 02:31:00 +0900

    feat: Add 채팅방 메시지 Redux 상태 관리 구현

--- bovo_client/src/store/index.js
+++ bovo_client/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logoutReducer from './logout/LogoutSlice';
-import bookReducer from "./bookForum/BookSlice"; // bookSlice import
+import logoutReducer from './logout/LogoutSlice.js';
+import bookReducer from "./bookForum/BookSlice.js"; // bookSlice import
+import chatReducer from "./chatInfo/ChatSlice.js";
 
 export const store = configureStore({
     reducer: {
         logout: logoutReducer,
-        book: bookReducer
+        book: bookReducer,
+        chat: chatReducer,
     }
 })
