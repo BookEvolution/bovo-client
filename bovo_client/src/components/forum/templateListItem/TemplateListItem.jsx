0c2bb00a5eb689d46e4ae7d3bc4cc36ab2d4b77a
Elvin-lee <herjun802@gmail.com>
2025-03-16 16:09:34 +0900

    feat: Add 독서 기록 공유 UI 구현 변경 및 공유 기능 연결

--- bovo_client/src/components/forum/templateListItem/TemplateListItem.jsx
+++ bovo_client/src/components/forum/templateListItem/TemplateListItem.jsx
@@ -1,65 +1,46 @@
 import PropTypes from "prop-types";
-import { Checkbox, ListItem, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Checkbox, Typography } from "@mui/material";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import styles from "./TemplateListItem.module.css";
 import { memo } from "react";
 
-const TemplateListItem = ({ checked, handleCheckboxChange }) => {
+const TemplateListItem = ({ checked, handleCheckboxChange, memo }) => {
     return (
-        <ListItem 
-            disablePadding
-            className={styles.templateListItem}
-            sx={{
-                display: "flex",  // ✅ flex 컨테이너 적용
-                justifyContent: "space-between",  // ✅ 요소 사이 간격 동일하게
-                alignItems: "center",
-                border: checked ? "2px solid #739CD4" : "none", // 체크 여부에 따라 border 색상 변경
-            }}
-        >
-            <Typography
-                sx={{
-                    width: "6.25rem",
-                    height: "3rem",
-                    borderRadius: "3.125rem",
-                    backgroundColor: "aliceblue",
-                    fontSize: "1.5rem",
-                    letterSpacing: "0.015rem",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center"
-                }}
+        <Accordion className={styles.templateListItem}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />} // 화살표 아이콘
+                aria-controls="panel1a-content"
+                id="panel1a-header"
             >
-                공통
-            </Typography>
-            <Typography
-                sx={{
-                    maxWidth: "20.625rem",
-                    maxHeight: "2.5rem",
-                    fontSize: "1.75rem",
-                    letterSpacing: "0.0175rem"
-                }}
-            >
-                책 읽은 계기가 무엇인가요?
-            </Typography>
-            <Checkbox
-                checked={checked}
-                onChange={handleCheckboxChange} // 체크박스 상태 변경 핸들러
-                icon={<CheckBoxOutlineBlankIcon sx={{ fontSize: "2rem" }} />} // ✅ 기본 체크박스 크기
-                checkedIcon={<CheckBoxIcon sx={{ fontSize: "2rem", color: "#739CD4" }} />} // ✅ 체크된 상태 크기
-                sx={{
-                    transition: "transform 0.3s ease-in-out, opacity 0.3s ease-in-out",
-                    "&.Mui-checked": {
-                        transform: "scale(1.2)",
-                        opacity: 1,
-                    },
-                    "&.Mui-unchecked": {
-                        transform: "scale(1)",
-                        opacity: 0.7,
-                    },
-                }}
-            />
-        </ListItem>
+                <Checkbox
+                    checked={checked}
+                    onChange={handleCheckboxChange}
+                    icon={<CheckBoxOutlineBlankIcon sx={{ fontSize: "2rem" }} />}
+                    checkedIcon={<CheckBoxIcon sx={{ fontSize: "2rem", color: "#739CD4" }} />}
+                />
+                <Typography
+                    sx={{
+                        fontSize: "1.75rem",
+                        letterSpacing: "0.0175rem",
+                    }}
+                >
+                    {memo.memo_Q} {/* 질문 표시 */}
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography
+                    sx={{
+                     fontSize: "1.5rem",
+                        letterSpacing: "0.0175rem",
+                        color: "#739CD4",
+                    }}
+                >
+                    {memo.memo_A} {/* 답변 표시 */}
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
     );
 };
 
@@ -68,4 +49,5 @@ export default memo(TemplateListItem);
 TemplateListItem.propTypes = {
     checked: PropTypes.bool.isRequired, // checked은 boolean 타입이고 필수 props
     handleCheckboxChange: PropTypes.func.isRequired, // handleCheckboxChange는 함수 타입이고 필수 props
+    memo: PropTypes.object.isRequired,  // memo는 객체 타입으로 변경
 };
