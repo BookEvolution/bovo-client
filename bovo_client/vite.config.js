85402e01c7db8d2c94f92e64b518120bbd659d0d
Elvin-lee <herjun802@gmail.com>
2025-04-14 14:19:45 +0900

    Feat: vite bundle analyzer 도입 lighthouse 성능 평가 이후 상세 분석을 위한 vite bundle analyzer 라이브러리 설치 및 도입

--- bovo_client/vite.config.js
+++ bovo_client/vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
     'global': 'window', // global을 window로 정의
   },
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // 분석 결과를 브라우저로 자동 오픈
+      filename: 'bundle-stats.html', // 생성될 파일 이름
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 })
