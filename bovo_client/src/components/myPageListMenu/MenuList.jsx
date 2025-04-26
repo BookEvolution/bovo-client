0e053ef84da51f7d045db97f26a355c71aea2896
Elvin-lee <herjun802@gmail.com>
2025-04-26 21:05:27 +0900

    Modify: menuList배열 type 추가 추후 메뉴 추가할 가능성에 따른 각 item의 type 프로퍼티를 추가함으로써 item 역할 명확히 함

--- bovo_client/src/components/myPageListMenu/MenuList.jsx
+++ bovo_client/src/components/myPageListMenu/MenuList.jsx
@@ -13,10 +13,10 @@ import styles from "./MenuList.module.css";
 import { Link } from "react-router-dom";
 
 const menuItem = [
-    { text: "내 프로필", icon: <AccountBoxIcon sx={{fontSize: "3rem"}} />, path: "/mypage/myprofile" },
-    { text: "독서 캘린더", icon: <EventIcon sx={{fontSize: "3rem"}} />, path: "/calendar" },
-    { text: "서비스 정보", icon: <InfoIcon sx={{fontSize: "3rem"}} />, path: "/mypage/service-info" },
-    { text: "로그 아웃", icon: <ExitToAppIcon sx={{fontSize: "3rem"}} />, path: "", action: "logout" },
+    { type: "link", text: "내 프로필", icon: <AccountBoxIcon sx={{fontSize: "3rem"}} />, path: "/mypage/myprofile" },
+    { type: "link", text: "독서 캘린더", icon: <EventIcon sx={{fontSize: "3rem"}} />, path: "/calendar" },
+    { type: "link", text: "서비스 정보", icon: <InfoIcon sx={{fontSize: "3rem"}} />, path: "/mypage/service-info" },
+    { type: "action", text: "로그 아웃", icon: <ExitToAppIcon sx={{fontSize: "3rem"}} />, action: "logout" },
 ];
 
 const MenuList = ({ onLogout }) => {
@@ -26,7 +26,7 @@ const MenuList = ({ onLogout }) => {
             sx={{ backgroundColor: "#E5F1FB", borderRadius: "1.625rem" }}
         >
             {menuItem.map((item, index) => (
-                item.action === "logout" ? (
+                item.type === "action" ? (
                     <ListItemButton 
                         key={index}
                         className={styles.menuBtn} 
