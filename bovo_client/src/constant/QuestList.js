dd43ba28c217c754b638ba72991f6ca619b7f8a1
Elvin-lee <herjun802@gmail.com>
2025-04-28 15:38:22 +0900

    Refactor: mission key 별도 파일 분리 기존 switch문으로 매칭했던 title과 key값들을 별도 파일로 분리

---
+++ bovo_client/src/constant/QuestList.js
@@ -0,0 +1,22 @@
+export const missionKeys = {
+    1: {
+        title: "출석",
+        countKey: "login_cnt",
+        completedKey: "is_login_completed",
+    },
+    2: {
+        title: "커뮤니티 참여",
+        countKey: "community_cnt",
+        completedKey: "is_community_completed",
+    },
+    3: {
+        title: "책 등록",
+        countKey: "book_reg_cnt",
+        completedKey: "is_book_reg_completed",
+    },
+    4: {
+        title: "독서 기록",
+        countKey: "note_cnt",
+        completedKey: "is_note_completed",
+    },
+};
